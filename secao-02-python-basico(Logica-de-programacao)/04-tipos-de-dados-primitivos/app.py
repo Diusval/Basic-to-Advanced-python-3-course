020940019e0d526d4c1617d41f809d72cec435fc
Diusval <79423948+Diusval@users.noreply.github.com>
2022-07-21 19:50:54 -0300

    Add files via upload

---
+++ secao-02-python-basico(Logica-de-programacao)/04-tipos-de-dados-primitivos/app.py
@@ -0,0 +1,36 @@
+"""
+TIPOS DE DADOS
+
+STR - STRING - TEXTOS 'Assim' "Assim"
+INT - INTEIRO - 12345 0 -10 -20 1500
+FLOAT - REAL / PONTO FLUTUANTE - 4.50 8.50 -240.30
+BOOL - BOOLEANO / LÓGICO - Thue/False 10 == 10
+
+"""
+
+"""
+print("Diusval", type("Diusval"))
+print(12, type(12))
+print(4.50, type(4.50))
+print(10 == 10, type(10 == 10))
+"""
+
+"""
+print("Diusval", type("Diusval"), bool("Diusval"))
+print("10", type("10"), type(int("10")))
+"""
+
+# Exercicio
+
+# Nome: string
+print("Diusval", type("Diusval"))
+
+# Idade: inteiro
+idade = 20
+idade_type = print(20, type(20))
+
+# Altura: Float
+print(1.80, type(1.80))
+
+# Maior de idade: Booleano
+print(idade >= 20, type(idade >= 20))
