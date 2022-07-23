3647b05c76099bc815d43288ca39cedfd044916e
Diusval <79423948+Diusval@users.noreply.github.com>
2022-07-22 21:05:37 -0300

    Add files via upload

---
+++ secao-02-python-basico(Logica-de-programacao)/05-Operadores-aritimeticos/app.py
@@ -0,0 +1,20 @@
+"""
++ - Soma
+- - Subtração
+* - Multiplicação
+/ - Divisão (Com resultado fracionário)
+// - Divisão (Com resultado inteiro)
+** - Exponencição ou Potenciação
+% - Modúlo de resto
+() - Prioridade de precedência
+"""
+
+
+print("Soma + ", 10 + 10)
+print("Subtração - ", 10 - 10)
+print("Multiplicação * ", 10 * 10)
+print("Divisão / ", 10 / 10)
+
+
+# print("Diusval" + " " + "Pinheiro e ele tem " + str(32) + " anos")
+
